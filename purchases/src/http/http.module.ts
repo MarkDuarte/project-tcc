38975e67e62366d5dd787f8398667393010e1bc0
MarkDuarte <marcosduarte1994@gmail.com>
2024-04-06 02:28:07 -0300

    create: Init project tcc

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'node:path';
+
+import { DatabaseModule } from '../database/database.module';
+import { TestResolver } from 'src/http/test.resolver';
+import { ApolloDriver } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gpl'),
+    }),
+  ],
+  providers: [TestResolver],
+})
+export class HttpModule {}
